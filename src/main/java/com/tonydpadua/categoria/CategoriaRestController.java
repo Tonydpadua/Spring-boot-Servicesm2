aa1723fd0d986effab5b948e6b9d16afbe13165f
Tony de Padua Castor Alves <11017073481@tjpb.jus.br>
2020-04-08 18:25:45 -0300

    Teste CategoriaRestController

---
+++ src/main/java/com/tonydpadua/categoria/CategoriaRestController.java
@@ -0,0 +1,16 @@
+package com.tonydpadua.categoria;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaRestController {
+
+    @GetMapping
+    public String teste(){
+        return "REST está funcionando";
+    }
+
+}
