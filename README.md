d3ae0b2a51d88689cb3590430b195a96b6170d7d
Tony de Padua Castor Alves <11017073481@tjpb.jus.br>
2020-07-15 21:59:37 -0300

    README

---
+++ README.md
@@ -0,0 +1,19 @@
+# API 
+
+## Visão geral
+
+Backend
+
+## Arquitetura do produto
+* Aplicação Spring Boot 2.3.1.RELEASE
+* Java 11
+* Tomcat 7
+
+### Dependências
+
+* Árvore de dependências maven [link](./mvn-dependency-tree.txt)
+
+
+## Diagrama
+
+![Screenshot](screenshot.png)
--- src/main/java/com/tonydpadua/config/SecurityConfig.java
+++ src/main/java/com/tonydpadua/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.tonydpadua.config;
 import com.tonydpadua.security.JWTAuthenticationFilter;
 import com.tonydpadua.security.JWTAuthorizationFilter;
 import com.tonydpadua.security.JWTUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
